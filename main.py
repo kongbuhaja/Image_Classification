1b29910ccd85b8be5006a420f470398910f96c15
kongbuhaja <pcar530@gmail.com>
2025-03-31 21:15:13 +0900

    rollback gpu

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import torch
 
 def train_process(args):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    device = "cpu"
 
     train_loader, eval_loader, test_loader, nc = data.get_dataloader(args,
                                                                      split='all')
@@ -19,7 +18,6 @@ def train_process(args):
 
 def eval_process(args):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    device = "cpu"
 
     test_loader, _ = data.get_dataloader(args,
                                          split='test')
